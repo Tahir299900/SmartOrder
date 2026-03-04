46196c4fe11a2a3688e4b55cca086ea06d7a2c38
Tahir299900 <tahir999992@gmail.com>
2026-03-04 01:46:50 +0000

    mysql db migrated to aws rds mysql

--- restaurant_system/settings.py
+++ restaurant_system/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'employees',
     'analytics',
     'customers',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -84,13 +85,24 @@ WSGI_APPLICATION = 'restaurant_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'restaurant_db',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'restaurant_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
+        'USER': 'admin',
+        'PASSWORD': 'password123',
+        'HOST': 'restaurant-db.cibewq8u6rio.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
